c6544a637a11457448fb8365ca2743967f0f0edc
Gevaerrd <gevaerrd@hotmail.com>
2025-04-28 16:02:32 -0300

    Primeira sessão frontend completa, segunda em desenvolvimento

---
+++ src/main/java/RLEnterprise/RlEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package RLEnterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlEnterpriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlEnterpriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RLEnterprise/RLEnterprise/RlEnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package RLEnterprise.RLEnterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlEnterpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
